439d6b8980c89b5dd8d5e8da1eef6c87645a8d4f
leomeden <leonardomeden@gmail.com>
2023-07-08 15:02:48 -0300

    first commit

---
+++ mongodb.js
@@ -0,0 +1,53 @@
+const { MongoClient } = require('mongodb');
+const path = require('path');
+
+require('dotenv').config({ path: path.join(__dirname, '.env')});
+
+const client = new MongoClient(process.env.DATABASE_URL);
+
+//Conexion con el cluster de MongoDB
+async function connect(){
+    console.log('Conectando...');
+    let connection = null;
+
+    try {
+        connection = await client.connect();
+        console.log('\tConectado');
+    } catch (error) {
+        console.log(error.message);
+    } 
+    
+    return connection;
+}
+
+//Desconexion del cluster de MongoDB
+async function disconnect(){
+   
+    try {
+        await client.close();
+        console.log('\tDesconectado');
+    } catch (error) {
+        console.log(error.message);
+    }
+    
+}
+
+//Conexion con la coleccion requerida
+async function connectToDb(collectionName) {   
+    const connection = await connect();
+    //console.log(process.env.DATABASE_NAME);
+    const db = connection.db(process.env.DATABASE_NAME);
+    const collection = db.collection(collectionName);
+
+    return collection;
+}
+
+//Conexion con la coleccion requerida
+async function generateID(collection) {   
+    const documentMaxId = await collection.find().sort({ id: -1}).limit(1).toArray();
+    const maxId = documentMaxId[0]?.id ?? 0;
+    
+    return maxId + 1;
+}
+
+module.exports = { connectToDb, disconnect, generateID }
---
+++ src/server.js
@@ -0,0 +1,124 @@
+
+const express = require("express");
+const server = express();
+
+const { connectToDb, disconnect, generateID} = require('../mongodb.js');
+
+// Middleware: Establece el manejo de datos en formato JSON
+/*You NEED express.json() and express.urlencoded() for POST and PUT requests, 
+because in both these requests you are sending data (in the form of some data object)
+to the server and you are asking the server to accept or store that data (object), */
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+
+// Obtener coches con params en forma dinamica: 
+// Ruta GET http://127.0.0.1:3000/coches
+server.get('/coches/', async (req, res) => {
+    const { marca, modelo, precio_mayor_que } = req.query;
+    const filtros = {};
+
+    if (marca) filtros.marca = marca;
+    if(modelo) filtros.modelo = modelo;
+    if(precio_mayor_que) filtros.precio = { $gt: +precio_mayor_que}
+
+    let coches = [];
+
+    const collection = await connectToDb('coches');
+    coches = (await collection.find(filtros).sort({ id: 1 }).toArray());
+    await disconnect();
+
+    if(coches.length == 0) return res.status(400).send('Error. No hay coches con la descripción dada');
+    res.status(200).send(JSON.stringify(coches, null, '\t'));
+
+});
+
+// Obtener un coche: Ruta GET http://127.0.0.1:3000/coches
+server.get('/coches/:id', async (req, res) => {
+    const { id } = req.params;
+
+    const collection = await connectToDb('coches');
+    const coche = await collection.findOne({ id: +id });
+
+    await disconnect();
+
+    if(!coche) return res.status(400).send('Error. No hay coches con ese id');
+    res.status(200).send(JSON.stringify(coche, null, '\t'));
+
+});
+
+// Obtener un coche: Ruta GET http://127.0.0.1:3000/coches
+server.post('/coches', async (req, res) => {
+    const { marca, modelo, anio, precio, descuento, velocidad_crucero, es_0km } = req.body;
+
+    if(!marca || !modelo || !anio || !precio) {
+        return res.status(400).send('Error. Los datos no estan completos!!')
+    }
+
+    const collection = await connectToDb('coches');
+    const coche = { id: await generateID(collection), marca, modelo, anio, precio};
+    if(descuento) coche.descuento = descuento;
+    if(velocidad_crucero) coche.velocidad_crucero = velocidad_crucero;
+    if(es_0km) coche.es_Okm = es_0km;
+
+    await collection.insertOne(coche);
+
+    await disconnect();
+
+    res.status(200).send(JSON.stringify(coche, null, '\t'));
+
+});
+
+// Modificar un coche: Ruta GET http://127.0.0.1:3000/coches
+server.put('/coches/:id', async (req, res) => {
+    const { id } = req.params
+    const { marca, modelo, anio, precio, descuento, velocidad_crucero, es_0km } = req.body;
+    const coche = { marca, modelo, anio, precio};
+
+    if(!id || !marca || !modelo || !anio || !precio) {
+        return res.status(400).send('Error. Los datos no estan completos!!')
+    }
+
+    if(descuento) coche.descuento = descuento;
+    if(velocidad_crucero) coche.velocidad_crucero = velocidad_crucero;
+    if(es_0km) coche.es_Okm = es_0km;
+
+    try {
+        const collection = await connectToDb('coches');
+        await collection.updateOne({ id: +id }, { $set: coche });
+        res.status(200).send(JSON.stringify(coche, null, '\t'));
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send('Error en el servidor');
+    } finally {
+        await disconnect();
+    }
+});
+
+// Borrar un coche: Ruta GET http://127.0.0.1:3000/coches
+server.delete('/coches/:id', async (req, res) => {
+    const { id } = req.params
+    
+    try {
+        const collection = await connectToDb('coches');
+        await collection.deleteOne({ id: +id });
+        res.status(200).send('Eliminado');
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send('Error en el servidor');
+    } finally {
+        await disconnect();
+    }
+});
+
+
+// Control de rutas inexistentes
+server.use('*', (req, res) => {
+    res.status(404).send(`<h1>Error 404</h1><h3>La URL indicada no existe en este servidor</h3>`);
+});
+
+// Método oyente de peteciones
+server.listen(process.env.SERVER_PORT, process.env.SERVER_HOST, () => {
+    console.log(`Ejecutandose en http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}/coches`);
+});
